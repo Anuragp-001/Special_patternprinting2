d28d9c3584637cba15168a95d9ad989058a1d934
Anurag Pandey <163474902+Anuragp-001@users.noreply.github.com>
2024-06-11 23:56:24 +0530

    Add files via upload

---
+++ StarReverseTriangle.java
@@ -0,0 +1,17 @@
+package PatternPrinting.SQUARE.ReverseTriangle;
+
+import java.util.Scanner;
+
+public class StarReverseTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter N : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= n+1-i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
